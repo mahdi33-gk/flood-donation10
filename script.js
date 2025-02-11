eb820bf6b409dd32a972e9a1f5839644a0a4b09d
mehedi <mehedi25122003@gmail.com>
2025-02-11 17:56:05 +0600

    commonn funnctio foe

---
+++ script.js
@@ -0,0 +1,9 @@
+function getInput(id){
+    const inputValue = parseFloat(document.getElementById(id).value);
+    return inputValue;
+}
+
+function getInnerText(id){
+    const getInnerText = parseFloat(document.getElementById(id).innerText);
+    return getInnerText;
+}
