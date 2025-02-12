e98a566f8ddc6670f90f92decdddc762a6746707
mehedi <mehedi25122003@gmail.com>
2025-02-12 15:26:40 +0600

    done / problem solved

--- commonFunction.js
+++ commonFunction.js
@@ -1,29 +1,13 @@
 function getInput(id) {
-  const inputValue = parseFloat(document.getElementById(id).value);
-  return inputValue;
+  const inputValue =(document.getElementById(id).value);
+  const nmbrInput = parseInt(inputValue);
+  return nmbrInput;
 }
 
 function getInnerText(id) {
-  const getInerText = parseFloat(document.getElementById(id).innerText);
-  return getInerText;
+  const getInerText = (document.getElementById(id).innerText);
+  const nmbrText = parseInt(getInerText);
+  return nmbrText;
 }
 
 
-
-clickBtn.addEventListener("click", function () {
-  const newCardBalance = cardOldBalance + inputDonationAmmount;
-  document.getElementById(cardBalance).innerText = newCardBalance;
-
-  const newMainbalance = oldBalance - inputDonationAmmount;
-  document.getElementById(mainBalance).innerText = newMainbalance;
-
-  function masterPiece(donationAmmount, cardBalance, mainBalance, btnClick) {
-    const oldBalance = getInnerText(mainBalance);
-    const cardOldBalance = getInnerText(cardBalance);
-    const inputDonationAmmount = getInput(donationAmmount);
-    const clickBtn = document.getElementById(btnClick);
-  
-    
-  }
-  masterPiece("noakhali-balanceInput", "noakhali-balance", "main-balance", "noakhali-balance-btns");
-});
--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-masterPiece("noakhali-balanceInput", "noakhali-balance", "main-balance", "noakhali-balance-btns");
+document.getElementById("noakhali-balance-btn").addEventListener("click", function () {
+    
+
+    const oldBalance = getInnerText("main-balance");
+    const cardOldBalance = getInnerText("noakhali-balance");
+    const inputDonationAmmount = getInput("noakhali-balanceInput");
+    const clickBtn = document.getElementById("noakhali-balance-btn");
+
+    const newCardBalance = cardOldBalance + inputDonationAmmount;
+    document.getElementById("noakhali-balance").innerText = newCardBalance;
+
+    const newMainbalance = oldBalance - inputDonationAmmount;
+    document.getElementById("main-balance").innerText = newMainbalance;
+
+    console.log(inputDonationAmmount);
+
+  });
+
+
